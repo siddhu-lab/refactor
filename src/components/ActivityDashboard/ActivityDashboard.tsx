ef4ba578ca82cb5e2260976b8a11b4c660a43cfc
siddhu-lab <66205473+siddhu-lab@users.noreply.github.com>
2025-07-30 10:05:16 -0400

    Fix useRef import error

--- src/components/ActivityDashboard/ActivityDashboard.tsx
+++ src/components/ActivityDashboard/ActivityDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import { initializeCharts } from './chartUtils.ts';
 import StatisticsTable from './StatisticsTable.tsx';
 import MainDataTable from './MainDataTable/MainDataTable.tsx';
