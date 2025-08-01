fa3f511c5476a23f29748ff9a62c92c5819c4e53
siddhu-lab <66205473+siddhu-lab@users.noreply.github.com>
2025-07-31 20:22:03 -0400

    Create unified intuitive graph dashboard with Chakra UI

--- src/components/MainDashboard.tsx
+++ src/components/MainDashboard.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Home, BookOpen, TrendingUp, MessageSquare, Lightbulb, Activity, Layers } from 'lucide-react';
-import UnifiedDashboard from './UnifiedDashboard/UnifiedDashboard.tsx';
+import UnifiedGraphDashboard from './UnifiedGraphDashboard/UnifiedGraphDashboard.tsx';
 
 
 const MainDashboard = () => {
@@ -23,7 +23,7 @@ const MainDashboard = () => {
     
     switch (active) {
       case 'Activity':
-        return <UnifiedDashboard />;
+        return <UnifiedGraphDashboard />;
       case 'Dictionary':
         return <div className="content-placeholder">Word Dictionary Component</div>;
       case 'Progress':
--- src/components/UnifiedGraphDashboard/UnifiedGraphDashboard.tsx
+++ src/components/UnifiedGraphDashboard/UnifiedGraphDashboard.tsx
@@ -246,8 +246,8 @@ const UnifiedGraphDashboard: React.FC = () => {
   };
 
   return (
-    <Box p={6} bg={useColorModeValue('gray.50', 'gray.900')} w="100%" h="100%">
-      <VStack spacing={6} align="stretch" h="100%">
+    <Box p={6} bg={useColorModeValue('gray.50', 'gray.900')} minH="100vh">
+      <VStack spacing={6} align="stretch">
         {/* Header */}
         <Card>
           <CardHeader>
@@ -392,9 +392,9 @@ const UnifiedGraphDashboard: React.FC = () => {
         </Card>
 
         {/* Main Content */}
-        <HStack spacing={6} align="start" flex="1" minH="0">
+        <HStack spacing={6} align="start">
           {/* Graph */}
-          <Card flex="1" h="100%" minH="500px">
+          <Card flex="1" minH="600px">
             <CardHeader>
               <HStack justify="space-between">
                 <Heading size="md">
@@ -405,20 +405,20 @@ const UnifiedGraphDashboard: React.FC = () => {
             </CardHeader>
             <CardBody>
               {loading ? (
-                <Center h="100%" minH="400px">
+                <Center h="500px">
                   <VStack>
                     <Spinner size="xl" />
                     <Text>Loading graph data...</Text>
                   </VStack>
                 </Center>
               ) : (
-                <Box ref={networkRef} h="100%" minH="400px" border="1px" borderColor={borderColor} borderRadius="md" />
+                <Box ref={networkRef} h="500px" border="1px" borderColor={borderColor} borderRadius="md" />
               )}
             </CardBody>
           </Card>
 
           {/* Sidebar */}
-          <VStack spacing={4} w="300px" h="100%" overflowY="auto">
+          <VStack spacing={4} w="300px">
             {/* Stats */}
             <Card w="full">
               <CardHeader>
